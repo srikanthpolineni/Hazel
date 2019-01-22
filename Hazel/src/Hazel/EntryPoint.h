9378cdee20c5990b557051464172d2fe5a50dc15
Srikanth Polineni <Polineni.Srikanth@gmail.com>
2019-01-22 23:35:01 +0530

    Added logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_ERROR("Error!!!!!!");
+	HZ_CORE_WARN("Warning!!!!!!");
+	HZ_CORE_INFO("Info!!!!!!");
+
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
